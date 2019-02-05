74996c12751691315a34d6a396865843d53b6ee2
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2019-02-05 18:06:30 +0200

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -5,15 +5,16 @@ class Perceptron:
     def __init__(self, no_of_inputs, epochs=100, learning_rate=0.01):
         self.epochs = epochs
         self.learning_rate = learning_rate
-        self.weights = np.zeros(no_of_inputs + 1) # creating array of zeros equal to the number of inputs + 1
-                                                  
+        self.weights = np.zeros(no_of_inputs + 1) # creating array of zeros equal
+                                                  # to the number of inputs + 1
+           
     def predict(self, inputs): # activation function predicting the output
-        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        summation = np.sum(np.dot(inputs, self.weights[1:])) + self.weights[0]
         if summation > 0:
             return 1
         else:
             return 0
-
+        
     def train(self, training_inputs, labels): # weights update function
         for _ in range(self.epochs):
             # training_inputs is expected to be a list made up
@@ -27,7 +28,7 @@ class Perceptron:
                 self.weights[0] += self.learning_rate * (label - prediction)
 
 # Creating a list of 2500 zero arrays
-training_inputs = [np.array([0]) for i in range(2500)]
+training_inputs = np.array([0 for i in range(2500)])
 
 with open("training_img_data.txt", "r") as f:
     # This is required to clean up the string from
@@ -38,8 +39,9 @@ with open("training_img_data.txt", "r") as f:
 labels = np.array([int(labels[i]) for i in range(len(labels))])
 
 # Creating Perceptron object and training it
-perceptron = Perceptron(1)
+perceptron = Perceptron(2500)
 perceptron.train(training_inputs, labels)
+print(len(perceptron.weights))
 
 # Forming test image data for perceptron to try to predict it
 with open("testing_img_data.txt", "r") as f:
@@ -50,6 +52,6 @@ with open("testing_img_data.txt", "r") as f:
 test_img_pixels = [int(test_img_pixels[i]) for i in range(len(test_img_pixels))]
 
 # Creating a list of arrays taht should be passed as an input to predict function
-test_input = [np.array([i]) for i in test_img_pixels]
+test_input = np.array([test_img_pixels])
 
-perceptron.predict(test_input)
+print(perceptron.predict(test_input))
